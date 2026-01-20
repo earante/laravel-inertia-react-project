c7c0d7dc416b328740cfa72537460ee9e076a4bd
Takamea2022 <arante.emmanuel2017@gmail.com>
2026-01-21 03:38:36 +0800

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="tittle">Hello User!</h1>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.jsx",
+    "./resources/**/*.js",
+    "./resources/**/*.tsx",
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
