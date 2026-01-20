3eaba0a0d60997ec4aed5ccafbeb476b631d4c97
Takamea2022 <arante.emmanuel2017@gmail.com>
2026-01-21 03:46:00 +0800

    feat: Added Paragraph on Home.jsx

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <>
             <h1 className="tittle">Hello User!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur esse et id necessitatibus neque nobis voluptates. Architecto asperiores, commodi dicta dolorem facilis fuga ipsam quas quos reiciendis? Dolores, sint tempora.</p>
         </>
     )
 }
